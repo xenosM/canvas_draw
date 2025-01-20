2882fe468179ae108ad18adf66511147f66df3bc
xenosM <105599352+xenosM@users.noreply.github.com>
2025-01-20 19:21:47 +0545

    Project Start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MainPage from "./pages/main_page"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <MainPage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Main_page.jsx
@@ -0,0 +1,8 @@
+const MainPage = function(){
+    return(
+        <main id="main-section">
+
+        </main>
+    )
+}
+export default MainPage
